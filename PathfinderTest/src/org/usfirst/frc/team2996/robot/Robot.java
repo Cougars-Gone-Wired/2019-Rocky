4d4bec0a78a8ff7877dbf4f803d87a5e29c971ab
tiegu <tiegu@192.168.0.6>
2018-11-24 01:59:44 -0700

    Added rebuilt pathfinding code

--- PathfinderTest/src/org/usfirst/frc/team2996/robot/Robot.java
+++ PathfinderTest/src/org/usfirst/frc/team2996/robot/Robot.java
@@ -4,10 +4,12 @@ import java.io.File;
 
 import com.ctre.phoenix.motorcontrol.SensorCollection;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.followers.DistanceFollower;
@@ -23,75 +25,98 @@ public class Robot extends IterativeRobot {
 	SpeedControllerGroup leftMotors, rightMotors;
 	SensorCollection leftSensors, rightSensors;
 	
-	Encoders encoder;
-	IMU gyro;
+	AHRS gyro;
+	Encoders encoders;
 	
-	File leftFile = new File("/home/lvuser/paths/Test_left_Jaci.csv"); // ./Main_left_Jaci.csv
-	File rightFile = new File("/home/lvuser/paths/Test_right_Jaci.csv"); // ./Main_right_Jaci.csv
+	File leftFile = new File("/home/lvuser/paths/LowTimeStep_left.csv");
+	File rightFile = new File("/home/lvuser/paths/LowTimeStep_right.csv");
+	File sourceFile = new File("/home/lvuser/paths/LowTimeStep_source.csv");
 	Trajectory leftTrajectory = Pathfinder.readFromCSV(leftFile);
 	Trajectory rightTrajectory = Pathfinder.readFromCSV(rightFile);
+	Trajectory sourceTrajectory = Pathfinder.readFromCSV(sourceFile);
 	DistanceFollower left;
 	DistanceFollower right;
-	double wheelbase = 1.9791667; // Feet
-	double wheelDiameter = 0.5; // Feet
-	double maxVel = 9.256463818; // Feet/second
-	double maxAccl = 9.84251969; // Feet/second^2
-	double maxJerk = 196.850394; // Feet/second^3
-
-	Notifier autoNotifier;
+	DistanceFollower source;
+	double P = 1;
+	double I = 0;
+	double D = 0;
+	double maxVelocity = 2.0;
+	double accelerationGain = 0.0;
+	
 	
 	@Override
 	public void robotInit() {
-	    frontLeftMotor = new WPI_TalonSRX(FRONT_LEFT_MOTOR_ID);
-	    rearLeftMotor = new WPI_TalonSRX(REAR_LEFT_MOTOR_ID);
-	    frontRightMotor = new WPI_TalonSRX(FRONT_RIGHT_MOTOR_ID);
-	    rearRightMotor = new WPI_TalonSRX(REAR_RIGHT_MOTOR_ID);
-	    
+		frontLeftMotor = new WPI_TalonSRX(FRONT_LEFT_MOTOR_ID);
+		rearLeftMotor = new WPI_TalonSRX(REAR_LEFT_MOTOR_ID);
+		frontRightMotor = new WPI_TalonSRX(FRONT_RIGHT_MOTOR_ID);
+		rearRightMotor = new WPI_TalonSRX(REAR_RIGHT_MOTOR_ID);
+		
 		leftMotors = new SpeedControllerGroup(frontLeftMotor, rearLeftMotor);
-	    rightMotors = new SpeedControllerGroup(frontRightMotor, rearRightMotor);
-	    
+		rightMotors = new SpeedControllerGroup(frontRightMotor, rearRightMotor);
+		
 		leftSensors = new SensorCollection(frontLeftMotor);
 		rightSensors = new SensorCollection(frontRightMotor);
 		
-	    gyro = new IMU();
-	    encoder = new Encoders(this);
-	    
-	    left = new DistanceFollower(leftTrajectory);
-	    right = new DistanceFollower(rightTrajectory);
-	    left.configurePIDVA(3.0, 0.0, 0.5, 1 / maxVel, 0);
-	    right.configurePIDVA(3.0, 0.0, 0.5, 1 / maxVel, 0);
-
-	    autoNotifier = new Notifier(runAuton);
+		gyro = new AHRS(SPI.Port.kMXP);
+		encoders = new Encoders(this);
+		
+		left = new DistanceFollower(leftTrajectory);
+		right = new DistanceFollower(rightTrajectory);
+		source = new DistanceFollower(sourceTrajectory);
+		left.configurePIDVA(P, I, D, 1 / maxVelocity, accelerationGain);
+		right.configurePIDVA(P, I, D, 1 / maxVelocity, accelerationGain);
+		source.configurePIDVA(P, I, D, 1 / maxVelocity, accelerationGain);
 	}
 
 	@Override
 	public void autonomousInit() {
-		autoNotifier.startPeriodic(0.2);
+		encoders.reset();
+		gyro.reset();
+		left.reset();
+		right.reset();
+		source.reset();
 	}
 
 	@Override
 	public void autonomousPeriodic() {
-
+		SmartDashboard.putNumber("leftEncoder", encoders.getLeftDistanceFeet());
+		SmartDashboard.putNumber("right encoder", encoders.getRightDistanceFeet());
+		
+		runStraight();
+		//runCurvy();
+		
+		if(left.isFinished()) {
+			System.out.println("left is finished");
+		} 
+		if (right.isFinished()) {
+			System.out.println("right is finished");
+		}
 	}
 
-	@Override
-	public void teleopInit() {
-
-	}
-	
 	@Override
 	public void teleopPeriodic() {
 		
 	}
-
-	@Override
-	public void disabledInit() {
-
+	
+	public void runStraight() {
+		double l = left.calculate(encoders.getLeftDistanceFeet());
+		double r = right.calculate(encoders.getRightDistanceFeet());
+		leftMotors.set(-l);
+		rightMotors.set(r);
 	}
-
-	@Override
-	public void disabledPeriodic() {
-
+	
+	public void runCurvy() {
+		double l = left.calculate(encoders.getLeftDistanceFeet());
+		double r = right.calculate(encoders.getRightDistanceFeet());
+		
+		double gyroHeading = gyro.getAngle();
+		double desiredHeading = Pathfinder.r2d(left.getHeading());
+		
+		double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
+		double turn = 0.8 * (-1.0 / 80.0) * angleDifference;
+		
+		leftMotors.set(-(l + turn));
+		rightMotors.set(r - turn);
 	}
 	
 	public SensorCollection getLeftSensors() {
@@ -101,25 +126,4 @@ public class Robot extends IterativeRobot {
 	public SensorCollection getRightSensors() {
 		return rightSensors;
 	}
-
-	Runnable runAuton = new Runnable() {
-		@Override
-		public void run() {
-			if (!left.isFinished() || !right.isFinished()) {
-				double l = left.calculate(encoder.getLeftDistanceFeet());
-				double r = right.calculate(encoder.getRightDistanceFeet());
-	  
-				double gyro_heading = gyro.getAngle();
-				double desired_heading = Pathfinder.r2d(left.getHeading());
-	  
-				double angleDifference = 0.8 * (-1.0 / 80.0) * Pathfinder.boundHalfDegrees(desired_heading - gyro_heading);
-				double turn = 0.8 * (-1.0 / 80.0) * angleDifference;
-	  
-				leftMotors.set(0.5 * (l + turn));
-				rightMotors.set(0.5 * (r - turn));
-			} else {
-				System.out.println("Done!!");
-			}
-		}
-	};
 }
